02578f25d0f17835243b6bd77b1826e2ae6c0ab7
Marcos Rivera <mjrivera@knowmadmood.com>
2026-07-13 11:22:12 +0200

    feat:  añadido un input en AppHeader para que el parametro del contacto fuera totalmente dinámico

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -3,7 +3,16 @@
         <nav class="mx-auto flex items-center gap-6 p-6">
             <nuxt-link to="/contactos">Contactos</nuxt-link>
             <nuxt-link to="/contactos/contacto">Contacto</nuxt-link>
-            <nuxt-link to="/contactos/123">Contacto parámetro</nuxt-link>
+            <div>
+                <span>Contacto parámetro: </span>
+                <input v-model="idParametro" type="number" class="border border-gray-300 rounded px-2 py-1 w-20 ml-2" />
+                <nuxt-link :to="link" class="border border-gray-300 rounded px-2 py-1 w-20 ml-2"> Ir </nuxt-link>
+            </div>
         </nav>
     </header>
 </template>
+
+<script setup lang="ts">
+    const idParametro = ref(123)
+    const link = computed(() => `/contactos/${idParametro.value}`)
+</script>
